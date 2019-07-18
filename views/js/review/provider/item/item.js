bf95fc0c19530713ad93bb8ab57c39cf7ab7472d
Hanna Dzmitryieva <bziondik@gmail.com>
2019-07-18 17:54:50 +0300

    get testMap, testContext, testData from init proxy

--- views/js/review/provider/item/item.js
+++ views/js/review/provider/item/item.js
@@ -36,6 +36,66 @@ define([
     //provider name
     qtiItemReviewer.name = 'qtiItemReview';
 
-    return qtiItemReviewer;
+    qtiItemPreviewer.init() = function {
+        const dataHolder = this.getDataHolder();
+        const areaBroker = this.getAreaBroker();
 
+        areaBroker.setComponent('toolbox', toolboxFactory());
+        areaBroker.getToolbox().init();
+
+        /*
+         * Install behavior on events
+         */
+        this
+            .on('ready', () => {
+                const itemIdentifier = dataHolder.get('itemIdentifier');
+                const itemData = dataHolder.get('itemData');
+
+                if (itemIdentifier) {
+                    if (itemData) {
+                        this.renderItem(itemIdentifier, itemData);
+                    } else {
+                        this.loadItem(itemIdentifier);
+                    }
+                }
+            })
+            .on('loaditem', (itemRef, itemData) => {
+                dataHolder.set('itemIdentifier', itemRef);
+                dataHolder.set('itemData', itemData);
+            })
+            .on('renderitem', () => {
+                this.trigger('enabletools enablenav');
+            })
+            .on('resumeitem', () => {
+                this.trigger('enableitem enablenav');
+            })
+            .on('disableitem', () => {
+                this.trigger('disabletools');
+            })
+            .on('enableitem', () => {
+                this.trigger('enabletools');
+            })
+            .on('error', () => {
+                this.trigger('disabletools enablenav');
+            })
+            .on('finish leave', () => {
+                this.trigger('disablenav disabletools');
+                this.flush();
+            })
+            .on('flush', () => {
+                this.destroy();
+            });
+
+        return this.getProxy()
+            .init()
+            .then(data => {
+                dataHolder.set('itemIdentifier', data.itemIdentifier);
+                dataHolder.set('itemData', data.itemData);
+                dataHolder.set('testMap', data.testMap);
+                dataHolder.set('testContext', data.testContext);
+                dataHolder.set('testData', data.testData);
+            });
+    };
+
+    return qtiItemReviewer;
 });

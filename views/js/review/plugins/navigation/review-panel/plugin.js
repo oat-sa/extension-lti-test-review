fd367fb8979de49523ea268bc0441f0817ebcffc
Martin Nicholson <martin@taotesting.com>
2022-01-13 17:23:08 +0100

    chore: fix headers years

--- views/js/review/plugins/navigation/review-panel/plugin.js
+++ views/js/review/plugins/navigation/review-panel/plugin.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2019-22 Open Assessment Technologies SA ;
+ * Copyright (c) 2019-2022 Open Assessment Technologies SA ;
  */
 /**
  * @author Jean-Sébastien Conan <jean-sebastien@taotesting.com>

6a34c396f9672e0373bece64875c1d2d01a719bc
olga-kulish <olga.kulish@arcadia.spb.ru>
2022-03-15 18:01:21 +0300

    fix: migration for displayItemTooltip

---
+++ migrations/Version202203151451263291_ltiTestReview.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\ltiTestReview\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202203151451263291_ltiTestReview extends AbstractMigration
+{
+    private const CONFIG_FILE = 'ReviewPanel';
+
+    public function getDescription(): string
+    {
+        return 'Enable item tooltip for ReviewPanel';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $extension = $this->getExtension();
+        $config = $extension->getConfig(self::CONFIG_FILE);
+        $config['displayItemTooltip'] = false;
+        $extension->setConfig(self::CONFIG_FILE, $config);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $extension = $this->getExtension();
+        $config = $extension->getConfig(self::CONFIG_FILE);
+        unset($config['displayItemTooltip']);
+        $extension->setConfig(self::CONFIG_FILE, $config);
+    }
+
+    /**
+     * @throws ExtensionException
+     */
+    private function getExtension(): Extension
+    {
+        /** @var ExtensionsManager $extensionManager */
+        $extensionManager = $this->getServiceLocator()->get(ExtensionsManager::SERVICE_ID);
+
+        return $extensionManager->getExtensionById('ltiTestReview');
+    }
+}

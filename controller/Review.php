a755254140a1b0ea8fcf2480c95ebbb3a95a76d2
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2026-01-15 09:37:39 +0100

    fix: Review::getUserFullName return empty string when empty

--- controller/Review.php
+++ controller/Review.php
@@ -325,7 +325,7 @@ class Review extends tao_actions_SinglePageModule
     private function getUserFullName(): string
     {
         if ($this->isSubmissionReviewRequestMessageProvided()) {
-            return $this->ltiSession->getLaunchData()->getUserFullName();
+            return $this->ltiSession->getLaunchData()->getUserFullName() ?? '';
         }
 
         return '';
